288c3a5911f57b1198f3503564967bd9f8cf1234
andy911002 <48443745+andy911002@users.noreply.github.com>
2019-03-13 14:46:05 +0800

    Create 民國與西元的換算 .cpp

---
+++ 民國與西元的換算 .cpp
@@ -0,0 +1,10 @@
+#include  <iostream>  
+using namespace std;  
+  
+int main()  
+{  
+int N;                       //int (使用字元)  n
+cin >> N;                    //輸入 >>n
+cout << N-1911 << endl;      //輸出     n-1911
+  
+}
