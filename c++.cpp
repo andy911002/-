dcd8276c0fe10462f6e3061123e61ab93a8c484c
andy911002 <48443745+andy911002@users.noreply.github.com>
2019-03-13 14:35:29 +0800

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-
-
 using namespace std;
 int main()
  {
-cout <<"a003:許願\t通過比率'93%'"<< endl
-<<"\t^_^ "<< endl 
-<< "\t\"o\""<< endl
-<<"\t\\^o^/" ;
+cout <<"a003:許願\t通過比率'93%'"<< endl//輸出文字
+<<"\t^_^ "<< endl                      //輸出符號
+<< "\t\"o\""<< endl                    //輸出符號
+<<"\t\\^o^/" ;                         //輸出符號
 
 }
