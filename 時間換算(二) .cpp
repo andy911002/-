add4f5496a73118de8d0b549901e5ab0d1ad39e0
andy911002 <48443745+andy911002@users.noreply.github.com>
2019-03-17 18:46:02 +0800

    Create 時間換算(二) .cpp

---
+++ 時間換算(二) .cpp
@@ -0,0 +1,12 @@
+#include  <iostream>  
+  
+using namespace std;  
+int main()  
+{  
+  
+int h;          //int (使用字元)  h
+cin >>h;       //輸入 h (小時)        
+cout << h/24 <<" " <<h%24 << endl;   //輸出小時  / 24 =幾天   餘數=小時(剩下的小時)
+  
+ return 0;  
+}
